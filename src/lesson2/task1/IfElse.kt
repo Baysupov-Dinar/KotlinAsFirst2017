29d2da623090f6c1eb6b919eec803881a387180d
Baysupov-Dinar <dinar.baysupov@gmail.com>
2017-09-29 18:23:07 +0300

    Урок 2 таск 2 (почти)

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -1,7 +1,10 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
+import java.lang.Math.*
 
 /**
  * Пример
@@ -33,7 +36,16 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    if ((age % 100) in 5..20)
+        return "$age лет"
+    else if ((age % 10) == 1)
+        return "$age год"
+    else if ((age % 10) in 2..4)
+        return "$age года"
+    else return "$age лет"
+
+}
 
 /**
  * Простая
@@ -44,7 +56,17 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+
+    val halfWay = ((t1 * v1) + (t2 * v2) + (t3 * v3)) / 2
+    val firstPart = (t1 * v1)
+    val secondPart = (t2 * v2)
+    if (halfWay <= firstPart)
+        return (halfWay / v1)
+    else if (halfWay < (firstPart + secondPart))
+        return (t1 + ((halfWay - firstPart) / v2))
+    else return (t1 + t2 + ((halfWay - firstPart - secondPart) / v3))
+}
 
 /**
  * Простая
@@ -57,7 +79,15 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+    if ((kingX == rookX1 || kingY == rookY1) && (kingX == rookX2 || kingY == rookY2))
+        return 3
+    else if (kingX == rookX1 || kingY == rookY1)
+        return 1
+    else if (kingX == rookX2 || kingY == rookY2)
+        return 2
+    return 0
+}
 
 /**
  * Простая
@@ -71,7 +101,18 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
  */
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
-                          bishopX: Int, bishopY: Int): Int = TODO()
+                          bishopX: Int, bishopY: Int): Int {
+
+    val delX = (kingX - bishopX)
+    val delY = (kingY - bishopY)
+    if ((Math.abs(delX) == Math.abs(delY)) && (kingX == rookX || kingY == rookY))
+        return 3
+    if (Math.abs(delX) == Math.abs(delY))
+        return 2
+    if (kingX == rookX || kingY == rookY)
+        return 1
+    return 0
+}
 
 /**
  * Простая
