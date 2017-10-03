206ff37b56fcedd9bafbf5066a6d570fbdd3191b
Baysupov-Dinar <dinar.baysupov@gmail.com>
2017-10-03 23:45:32 +0300

    Урок 2 таск 2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,7 +1,10 @@
 @file:Suppress("UNUSED_PARAMETER")
 package lesson2.task2
 
+import lesson1.task1.sqRoot
 import lesson1.task1.sqr
+import java.lang.Math.abs
+import java.lang.Math.pow
 
 /**
  * Пример
@@ -17,7 +20,13 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val firstNum = number/1000
+    val secondNum = number/100 - firstNum*10
+    val thirdNum = number/10 - number/100*10
+    val fourthNum = number%10
+    return(firstNum+secondNum == thirdNum + fourthNum)
+}
 
 /**
  * Простая
@@ -26,7 +35,11 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    val delX = Math.abs(x2-x1)
+    val delY = Math.abs(y2-y1)
+    return (x1 == x2 || y1 == y2 || delX == delY)
+}
 
 /**
  * Средняя
@@ -36,7 +49,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    val distance = pow((sqr(x1 - x2) + sqr(y1 - y2)), 1 / 2.0)
+    return (distance + r1 <= r2)
+}
 
 /**
  * Средняя
@@ -47,4 +63,5 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+ (a <= r && b <= s) || (b <= r && a <= s) || (a <= r && c <= s) || (c <= r && a <= s) || (b <= r && c <= s) || (c <= r && b <= s)
